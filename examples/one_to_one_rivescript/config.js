e497cc21aa497eabd3fd0bb2b514764f97a5f7dc
Igor Khomenko <igor@quickblox.com>
2016-05-18 13:26:47 +0300

    added one_to_one_rivescript chat bot

---
+++ examples/one_to_one_rivescript/config.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Get application credentioaln on https://admin.quickblox.com
+//
+exports.CONFIG = {"appId": "13318",
+              "authKey": "WzrAY7vrGmbgFfP",
+              "authSecret": "xS2uerEveGHmEun",
+              "user_id": "2740296",
+              "user_password": "mehdoh00"}
